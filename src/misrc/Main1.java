ebedf59bb6008b72e3db88dfae0dc605fdadec75
Sergii <sergio.defrutos@campusfp.es>
2021-01-25 10:06:36 +0100

    2 SNAPSHOT Main1.java array double

--- src/misrc/Main1.java
+++ src/misrc/Main1.java
@@ -10,6 +10,12 @@ public class Main1 {
         for(int i=0; i<nombre.length; i++) {
             System.out.println(nombre[i]);
         }
+        //Declaración Array tipo DOUBLE
+        double[] estatura = {1.72, 1.75, 1.65};
+        //Recorrer el array
+        for(int i=0; i<estatura.length; i++){
+            System.out.println(estatura[i]);
+        }
     }
     
 }
