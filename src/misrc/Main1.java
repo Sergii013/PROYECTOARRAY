f4fefb7166227918d38e27b2b542984ff5da324c
Sergii <sergio.defrutos@campusfp.es>
2021-01-25 10:02:18 +0100

    1 SNAPSHOT Main1.java

---
+++ src/misrc/Main1.java
@@ -0,0 +1,15 @@
+package misrc;
+
+public class Main1 {
+
+    
+    public static void main(String[] args) {
+        //Declaración array
+        String[] nombre = {"Luis", "Miguel","Carlos"};
+        //Recorrer array
+        for(int i=0; i<nombre.length; i++) {
+            System.out.println(nombre[i]);
+        }
+    }
+    
+}
